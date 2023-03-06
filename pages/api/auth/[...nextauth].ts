14d05af5505d26bdd633ffdb945b0d61e35612f4
Ilem Santos <ilemlima@gmail.com>
2023-03-05 20:18:30 -0400

    Create adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,17 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
+import { Sequelize } from 'sequelize'
+import SequelizeAdapter from '@next-auth/sequelize-adapter'
+
+// const sequelize = new Sequelize("yourconnectionstring")
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_DATABASE_URL,
+  database: process.env.DB_DATABASENAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+});
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -14,6 +25,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     })
   ],
+  adapter: SequelizeAdapter(sequelize),
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
